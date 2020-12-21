d1a84d84fdc2631727ea932904cf1cef2b98faf5
NaziaToma <nazia.tabassum@northsouth.edu>
2020-12-21 10:50:21 +0600

    Practice #6 : xPath adeed to parse function

--- Code/Practice/demo_project/demo_project/spiders/jokes.py
+++ Code/Practice/demo_project/demo_project/spiders/jokes.py
@@ -1,5 +1,6 @@
 import scrapy 
-
+from demo_project.items import JokeItem
+from scrapy.loader import ItemLoader
 class JokesSpider(scrapy.Spider):
     name= 'jokes'
 
@@ -9,9 +10,9 @@ class JokesSpider(scrapy.Spider):
 
     def parse(self, response):
         for joke in response.xpath("//div[@class='jokes']"):
-            yield {
-                'joke_text': joke.xpath(".//div[@class='joke-text']/p").extract_first()
-            }
+            l= ItemLoader(item=JokeItem(),selector=joke)
+            l.add_xpath('joke_text', ".//div[@class='joke-text']/p")
+            yield l.load_item()
 
         next_page= response.xpath("//li[@class='next']/a/@href").extract_first()
         if next_page is not None:
