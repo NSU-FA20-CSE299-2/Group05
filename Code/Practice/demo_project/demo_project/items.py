c969fe9bafe802394c1fa54fead78f3221d48ae3
NaziaToma <nazia.tabassum@northsouth.edu>
2020-12-21 11:00:47 +0600

    Practice #6 : Removed all html tags

--- Code/Practice/demo_project/demo_project/items.py
+++ Code/Practice/demo_project/demo_project/items.py
@@ -4,7 +4,12 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
+from w3lib.html import remove_tags
 
 
 class JokeItem(scrapy.Item):
-    joke_text= scrapy 
+    joke_text= scrapy.Field(
+        input_processor= MapCompose(remove_tags),
+        output_processor= TakeFirst()
+    )
