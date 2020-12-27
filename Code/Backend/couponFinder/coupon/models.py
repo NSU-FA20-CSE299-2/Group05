05bdedc04628f3ece7eab421714d4d455af47180
NaziaToma <nazia.tabassum@northsouth.edu>
2020-12-27 21:24:23 +0600

    Enhancement #4 : coupon class added

--- Code/Backend/couponFinder/coupon/models.py
+++ Code/Backend/couponFinder/coupon/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class company(models.Model):
+class Company(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+class Coupon(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, blank=True, null=True)
+    name = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=500, null=True)
+    code = models.CharField(max_length=200, null=True)
 
 
     def __str__(self):
