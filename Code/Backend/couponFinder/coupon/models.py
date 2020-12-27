4556879b16e3391d2a0e6b2ed989dee7e88b5b88
NaziaToma <nazia.tabassum@northsouth.edu>
2020-12-27 21:11:02 +0600

    Enhancement #4 : company class added

--- Code/Backend/couponFinder/coupon/models.py
+++ Code/Backend/couponFinder/coupon/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class company(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+
+    def __str__(self):
+        return self.name
