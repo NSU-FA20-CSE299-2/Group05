485eac2dace18208b192e240a6097e8fc09bf56f
NaziaToma <nazia.tabassum@northsouth.edu>
2020-12-28 11:00:49 +0600

    Enhancement #4 : coupon function added to views.py for rendering

--- Code/Backend/couponFinder/coupon/views.py
+++ Code/Backend/couponFinder/coupon/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def coupon(request):
+    coupon = Coupon.objects.all()
+    context = {'coupon':coupon}
+    return render(request, 'Frontend/Backend/homepage.html')
