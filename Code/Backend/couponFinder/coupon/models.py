586a45d7e44d81fc316a7a28b711911b22e45683
NaziaToma <nazia.tabassum@northsouth.edu>
2021-01-18 15:04:15 +0600

    Enhancement #12 : new class created for fashion coupon

--- Code/Backend/couponFinder/coupon/models.py
+++ Code/Backend/couponFinder/coupon/models.py
@@ -48,3 +48,23 @@ class Gadget_coupon(models.Model):
         except:
             url = ''
         return url
+
+class Fashion_coupon(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, blank=True, null=True)
+    name = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=500, null=True)
+    code = models.CharField(max_length=200, null=True)
+    image= models.ImageField(null = True, blank= True)
+
+
+    def __str__(self):
+        return self.name
+
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
