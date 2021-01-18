0263d783ae1b8a392132ef908c1e8cc362338e07
NaziaToma <nazia.tabassum@northsouth.edu>
2021-01-18 11:57:40 +0600

    Enhancement #11 : path added for rendering

--- Code/Backend/couponFinder/coupon/urls.py
+++ Code/Backend/couponFinder/coupon/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.coupon, name="coupon"),
     path('food/', views.company, name="food"),
+    path('gadget/', views.gadget_coupon, name="gadget"),
 ]
