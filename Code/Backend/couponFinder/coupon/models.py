12e0df403e566f4d6aecbed0b9a9e01d081dc2db
NaziaToma <nazia.tabassum@northsouth.edu>
2021-01-18 11:56:37 +0600

    Enhancement #11 : class added for gadget_coupon

--- Code/Backend/couponFinder/coupon/models.py
+++ Code/Backend/couponFinder/coupon/models.py
@@ -28,3 +28,23 @@ class Coupon(models.Model):
         except:
             url = ''
         return url
+
+class Gadget_coupon(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, blank=True, null=True)
+    name = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=500, null=True)
+    code = models.CharField(max_length=200, null=True)
+    image= models.ImageField(null = True, blank= True)
+
+
+    def __str__(self):
+        return self.name
+
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
